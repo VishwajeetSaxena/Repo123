60a40653c426347e81391a7a81c5c864b780d243
Vish <asivish@gmail.com>
2018-08-06 09:07:21 +0530

    adding local repo to remote

---
+++ Job/A.java
@@ -0,0 +1,16 @@
+class A 
+{ 
+   public static void main(String args[])
+   { 
+      String str[] = {""}; 
+      System.out.print("hi"); 
+      B.main(str); 
+   } 
+} 
+class B 
+{ 
+   public static void main(String args[]) 
+   { 
+     System.out.println("hello");
+   }
+} 
